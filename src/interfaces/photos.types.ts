f19f254c07e3d399b032ea7c62b6adeff1f86b18
Davidev <davidlez2mgk@gmail.com>
2022-07-03 18:01:12 -0500

    🏷️ interfaces photos

--- src/interfaces/photos.types.ts
+++ src/interfaces/photos.types.ts
@@ -1,4 +1,18 @@
+type TypeErrorPhotosPlash = {
+	message: string;
+	isExistError: boolean;
+};
+
+export interface TypeEmptyStatePhotosPlash {
+	results: any[];
+	isLoading: boolean;
+	error: TypeErrorPhotosPlash;
+}
 export interface typeApiPhotosProps {
 	// setPhotos: Function;
 	successPhotos: Function;
+}
+
+export interface usePhotos {
+	photos: TypeEmptyStatePhotosPlash;
 }
