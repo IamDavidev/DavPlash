7166cbf3bb1e9a363806d6892e2820cc7ab29ff5
itsDavidev <davidlez2mgk@gmail.com>
2022-08-09 22:11:23 -0500

    first test

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,30 @@
+/**
+ * @ComondJS
+// module.exports = {
+// 	moduleNameMapper: {
+// 		'^@/(.*)$': '<rootDir>/src/$1',
+// 		'~components': '<rootDir>/src/components',
+// 		'~interfaces': '<rootDir>/src/interfaces',
+// 		'~lib': '<rootDir>/src/lib',
+// 		'~constants': '<rootDir>/src/constants',
+// 		'~pages': '<rootDir>/src/pages',
+// 	},
+// };
+
+/**
+ * @ESModule
+ */
 export {};
-module.exports = {
+export default {
 	moduleNameMapper: {
-		'^@/(.*)$': '<rootDir>/src/$1',
 		'~components': '<rootDir>/src/components',
 		'~interfaces': '<rootDir>/src/interfaces',
 		'~lib': '<rootDir>/src/lib',
 		'~constants': '<rootDir>/src/constants',
 		'~pages': '<rootDir>/src/pages',
+		'^@/(.*)$': '<rootDir>/src/$1',
 	},
+	testEnvironment: 'jsdom',
+	testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)(spec|test|tests).ts'],
+	setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
 };
