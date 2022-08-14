f08599ab3cd56bed7ca1de7955364ed2e13d2171
David Lezama <davidlez2mgk@gmail.com>
2022-08-14 16:02:27 -0500

    🚚 no Access Route

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,21 @@ import { Box } from '@chakra-ui/react';
 
 import '@/styles/global.styles.css';
 
-import { FC, Suspense } from 'react';
+import { FC, Suspense, useContext } from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 
 import LoadingSuspense from '~pages/layouts/LoadingSuspense.layout';
 import Nabvar from '~pages/layouts/Navbar.layout';
 
+import { LoggedInContext } from '~lib/context/loggenIn.context';
 import { useIsDarkMode } from '~lib/hooks';
 import PrivateRoutes from './routes/Private.routes';
 import PublicRoutes from './routes/Public.routes';
 
 const App: FC = () => {
 	const isDarkMode = useIsDarkMode();
-	const isLoggedIn: boolean = true;
+	const { loggedIn } = useContext(LoggedInContext);
 	return (
 		<Box
 			px={'10'}
@@ -25,7 +26,7 @@ const App: FC = () => {
 			<Suspense fallback={<LoadingSuspense />}>
 				<BrowserRouter>
 					<Nabvar />
-					{isLoggedIn ? (
+					{loggedIn ? (
 						<>
 							<PrivateRoutes />
 						</>
--- src/routes/Public.routes.tsx
+++ src/routes/Public.routes.tsx
@@ -1,29 +1,26 @@
+import { FC, lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import PageNotFound from '~pages/404.view';
 import HomeView from '~pages/Home.view';
-import Login from '~pages/Login.view';
 
-const PublicRoutes = () => {
+/**
+ * lazy loading in Pages
+ */
+
+const Login = lazy(() => import('~pages/Login.view'));
+const NoAccess = lazy(() => import('~pages/NoAccess.view'));
+const Page404 = lazy(() => import('~pages/404.view'));
+
+const PublicRoutes: FC = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Navigate to='/plash/' replace />} />
 			<Route path='/plash/' element={<HomeView />} />
 			<Route path={'/plash/login'} element={<Login />} />
-			<Route
-				path='/plash/discover/photos/*'
-				element={'no tienes acceso a esta ruta'}
-			/>
-			{/* create cuztomization page` No Access` */}
-			<Route
-				path='/plash/discover/collections/*'
-				element={'no tienes acceso a esta ruta'}
-			/>
-			<Route
-				path='/plash/discover/users/*'
-				element={'no tienes acceso a esta ruta'}
-			/>
-			<Route path='*' element={<PageNotFound />} />
+			<Route path='/plash/discover/photos/*' element={<NoAccess />} />
+			<Route path='/plash/discover/collections/*' element={<NoAccess />} />
+			<Route path='/plash/discover/users/*' element={<NoAccess />} />
+			<Route path='*' element={<Page404 />} />
 		</Routes>
 	);
 };
